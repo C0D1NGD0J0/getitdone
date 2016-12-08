d6afb0a9bd99c688aafab94808c09ef0ee4fdcf7
C0D1NGD0J0 <sam.gdouglas@gmail.com>
2016-12-08 13:54:12 +0000

    custom attributes added to devise + updated permitted params for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  devise_for :users, 
+  	path: '',
+  	path_names: {:sign_in => 'login', :sign_out => 'logout'},
+  	controllers: {registrations: 'registrations'}
 end
