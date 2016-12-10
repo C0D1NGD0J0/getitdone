966492ed645affd0eb4d5d2613fc4eb9dccff9c7
C0D1NGD0J0 <sam.gdouglas@gmail.com>
2016-12-10 11:23:37 +0000

    changed the homepage for logged_in users

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
 	def home
+		if current_user
+			redirect_to dashboard_path
+		end
+	end
+
+	def dashboard
+		@tasks = Task.all
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get	'dashboard' => 'pages#dashboard'
   devise_for :users, 
   	path: '',
   	path_names: {:sign_in => 'login', :sign_out => 'logout'},
