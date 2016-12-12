17b448284291e8242794d866bdd9efed40c741ff
C0D1NGD0J0 <sam.gdouglas@gmail.com>
2016-12-12 20:03:57 +0000

    twilio sms feature implemented v1

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   PHONE_REGEX = /\A[0-9]*\Z/
 
   validates :first_name, :last_name, :phone, presence: true, uniqueness: true
-  validates :phone, length: {minimum: 10, maximum: 12}
+  validates :phone, length: {minimum: 10, maximum: 18}
   validates_format_of :phone, with: PHONE_REGEX
   validates :first_name, :last_name, length: {within: 3..15}
 end
