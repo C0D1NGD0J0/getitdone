3dfa4cee1aebea95ff93919eb1fb7d6b550d2e4f
C0D1NGD0J0 <web-dev@United4Life.local>
2017-02-21 16:27:01 +0100

    updated tab-panel to display all task regardless of if its not today

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from(AbstractController::ActionNotFound) do
+    flash.now[:error] = 'Action Not FOund'
+    render root_url
+  end
+
+  # rescue_from AbstractController::ActionNotFound, with: -> { render_404  }
+  # rescue_from AbstractController::ActionNotFound, with: :render_404
+
+  # def render_404
+  # 	puts "INSIDE render_404" * 10
+  #   respond_to do |format|
+  #     format.html { render template: 'errors/not_found', status: 404 }
+  #   end
+  # end
 end
